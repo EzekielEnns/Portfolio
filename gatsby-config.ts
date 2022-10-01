dfa3d37eb0c2d7186cdf80e365666a72dd0b4aea
Ezekiel Enns <ezekielenns@pop-os.localdomain>
2022-10-01 18:25:33 -0300

    got a good style going and mdx pages are loading well

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,26 +9,39 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+      "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", 
+      {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+          "icon": "src/images/icon.png"
+        }
+      }, 
+      "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          "name": "images",
+          "path": "./src/images/"
+        },
+        __key: "images"
+      }, 
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          "name": "pages",
+          "path": "./src/pages/"
+        },
+        __key: "pages"
+      },
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              "name":"content",
+              "path":`${__dirname}/content/`
+          },
+      },
+  ],
 };
 
 export default config;
---
+++ gatsby-node.ts
@@ -0,0 +1,45 @@
+import * as path from "path"
+const template = path.resolve(`src/templates/template.tsx`)
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMdx {
+          nodes {
+            id,
+            frontmatter {
+              title
+              type
+            },
+            internal {
+              contentFilePath
+            }
+          }
+      },
+      allSitePage {
+        nodes {
+          path,
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    console.error(result.errors)
+  }
+  result.data.allMdx.nodes.forEach(node => {
+    let isIndex = node.frontmatter.type == "index" || node.frontmatter.title == 'index';
+    let isProject = node.frontmatter.type == "project";
+    let isAbout = node.frontmatter.type == "about";
+    let path = `/${isProject?'projects':''}${isAbout?'about':''}${!isIndex?`/${node.frontmatter.title}`:'/'}`;
+    console.log(path)
+    createPage({
+      path: path,
+      component:`${template}?__contentFilePath=${node.internal.contentFilePath}` ,
+      context: {
+        type: node.frontmatter.type, // "Using a Theme"
+        id: node.id
+      },
+    })
+  })
+}
