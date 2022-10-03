f2d37064a47322d063cae8032835626491fb8e58
Ezekiel Enns <ezekielenns@pop-os.localdomain>
2022-10-03 11:15:33 -0300

    removed dependency of template, finihsed css rules for template

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { Link, HeadFC } from "gatsby"
+import Template from "../templates/template"
 
 const pageStyles = {
-  color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
@@ -25,22 +25,17 @@ const codeStyles = {
 
 const NotFoundPage = () => {
   return (
+  <Template type="?">
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/">Go home</Link>
       </p>
     </main>
+  </Template>
   )
 }
 
