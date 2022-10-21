287dc90e9399fc4dc826b305281945b83c51e3bc
Ezekiel Enns <ezekielenns@pop-os.localdomain>
2022-10-21 12:12:34 -0300

    rebasing

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueMobileDetection from 'vue-mobile-detection'
-const app = createApp(App)
-app.use(VueMobileDetection,{});
-app.mount('#app')
