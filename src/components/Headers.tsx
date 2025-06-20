3c348ecfe52f47467c41601a14ae1112dd256d54
Zeke <ezekiel.enns@protonmail.com>
2025-06-20 16:16:32 -0600

    woot woot

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -1,119 +1,127 @@
-import { styled } from "@styled-system/jsx";
+import { Stack, styled } from "@styled-system/jsx";
 import Modal from "components/Modal";
 import { About } from "content";
 import { Box } from "layout/pandas/Box";
-import { useState } from "react";
-import { FiFileText } from "react-icons/fi";
+import { useState, type ReactNode } from "react";
+import { FiFileText, FiMail, FiPhone } from "react-icons/fi";
 import { GoPerson } from "react-icons/go";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
 
 export default function Headers() {
   const [about, setAbout] = useState(false);
 
   return (
-    <Box display={"flex"}>
-      <styled.h1 fontSize={"xx-large"}>Explore My Work</styled.h1>
-      <styled.div ml={"auto"} display={"flex"}>
-        <Box
-          display={"flex"}
-          flexDir={"column"}
-          border={"none"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          cursor={"pointer"}
-          onClick={() => {
-            setAbout((o) => !o);
-          }}
-          _hover={{
-            textDecoration: "underline",
-          }}
-        >
-          <GoPerson fontSize={"2em"} />
-          <styled.span fontSize={"sm"} textAlign={"center"}>
-            About
-          </styled.span>
-        </Box>
-        <Box
-          display={"flex"}
-          flexDir={"column"}
-          border={"none"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          cursor={"pointer"}
-          onClick={() => {
-            window.open("/resume.pdf", "_blank");
-          }}
-          _hover={{
-            textDecoration: "underline",
-          }}
-        >
-          <FiFileText fontSize={"2em"} />
-          <styled.span w={"100%"} fontSize={"sm"} textAlign={"center"}>
-            CV
-          </styled.span>
-        </Box>
-        <Box
-          display={"flex"}
-          flexDir={"column"}
-          border={"none"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          cursor={"pointer"}
-          onClick={() => {
-            window.open("https://www.linkedin.com/in/ezekiel-enns/", "_blank");
-          }}
-          _hover={{
-            textDecoration: "underline",
-          }}
-        >
-          <FaLinkedin fontSize={"2em"} />
-          <styled.span fontSize={"sm"} textAlign={"center"}>
-            LinkedIn
-          </styled.span>
-        </Box>
-        <Box
-          display={"flex"}
-          flexDir={"column"}
-          border={"none"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          cursor={"pointer"}
-          onClick={() => {
-            window.open("https://github.com/ezekielenns", "_blank");
-          }}
-          _hover={{
-            textDecoration: "underline",
-          }}
-        >
-          <FaGithub fontSize={"2em"} />
-          <styled.span fontSize={"sm"} textAlign={"center"}>
-            GitHub
-          </styled.span>
-        </Box>
-        <Box
-          display={"flex"}
-          flexDir={"column"}
-          border={"none"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          cursor={"pointer"}
-          onClick={() => {
-            window.open("mailto:ezekiel.enns@pm.me", "_self");
-          }}
-          _hover={{
-            textDecoration: "underline",
-          }}
-        >
-          <MdEmail fontSize={"2em"} />
-          <styled.span fontSize={"sm"} textAlign={"center"}>
-            Email
-          </styled.span>
-        </Box>
-      </styled.div>
+    <Box display={"flex"} flexDir={"column"} gap={3}>
+      <Box display={"flex"} alignItems={"center"}>
+        <styled.h1 fontSize={"xx-large"}>Explore My Work</styled.h1>
+        <Stack ml={"auto"} direction="row" gap={2}>
+          <LinkButton
+            name="About"
+            icon={<GoPerson fontSize={"2em"} />}
+            onClick={() => setAbout((o) => !o)}
+          />
+          <Link
+            name="CV"
+            url="/resume.pdf"
+            icon={<FiFileText fontSize={"2em"} />}
+          />
+        </Stack>
+      </Box>
+      <Box border={"none"} display={"flex"} gap={1} flexWrap={"wrap"}>
+        <CompactLink
+          name="LinkedIn"
+          url="https://www.linkedin.com/in/ezekiel-enns/"
+          icon={<FaLinkedin fontSize={"1.2em"} />}
+        />
+        <CompactLink
+          name="GitHub"
+          url="https://github.com/ezekiel-enns"
+          icon={<FaGithub fontSize={"1.2em"} />}
+        />
+        <CompactLink
+          name="Email"
+          url="mailto:ezekiel.enns@pm.me"
+          icon={<FiMail fontSize={"1.2em"} />}
+        />
+        <CompactLink
+          name="Phone"
+          url="tel:+14035986653"
+          icon={<FiPhone fontSize={"1.2em"} />}
+        />
+      </Box>
       <Modal expand={about} onClick={() => setAbout((o) => !o)}>
         <About />
       </Modal>
     </Box>
   );
 }
+type LinkProps = { name: string; icon: ReactNode; url: string };
+const Link = (props: LinkProps) => (
+  <Box
+    display={"flex"}
+    flexDir={"column"}
+    border={"none"}
+    justifyContent={"center"}
+    alignItems={"center"}
+    cursor={"pointer"}
+    onClick={() => {
+      window.open(props.url, "_blank");
+    }}
+    _hover={{
+      textDecoration: "underline",
+    }}
+  >
+    {props.icon}
+    <styled.span fontSize={"sm"} textAlign={"center"}>
+      {props.name}
+    </styled.span>
+  </Box>
+);
+
+type LinkButtonProps = { name: string; icon: ReactNode; onClick: () => void };
+const LinkButton = (props: LinkButtonProps) => (
+  <Box
+    display={"flex"}
+    flexDir={"column"}
+    border={"none"}
+    justifyContent={"center"}
+    alignItems={"center"}
+    cursor={"pointer"}
+    onClick={props.onClick}
+    _hover={{
+      textDecoration: "underline",
+    }}
+  >
+    {props.icon}
+    <styled.span fontSize={"sm"} textAlign={"center"}>
+      {props.name}
+    </styled.span>
+  </Box>
+);
+
+type CompactLinkProps = { name: string; icon: ReactNode; url: string };
+const CompactLink = (props: CompactLinkProps) => (
+  <Box
+    display={"flex"}
+    flexDir={"row"}
+    alignItems={"center"}
+    gap={2}
+    flex={1}
+    justifyContent={"center"}
+    cursor={"pointer"}
+    py={2}
+    px={3}
+    borderRadius={"md"}
+    onClick={() => {
+      window.open(props.url, "_blank");
+    }}
+    _hover={{
+      bg: "gray.100",
+    }}
+  >
+    {props.icon}
+    <styled.span fontSize={"xs"} textAlign={"center"}>
+      {props.name}
+    </styled.span>
+  </Box>
+);
